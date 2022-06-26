50977e8a6d2496b56e3f586dee4c670576f5915c
MadhaviImashi <madhaviimashi@gmail.com>
2022-06-26 13:52:30 +0530

    connect local mongodb using mongoose

--- backend/dal/index.js
+++ backend/dal/index.js
@@ -1,16 +1,25 @@
 // const mongodb = require('mongodb');
 const {MongoClient} = require('mongodb');
-const DbClient = new MongoClient('mongodb://localhost:27017', {
+const DbClient = new MongoClient('mongodb://localhost:27017/practice-final-exam-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 DbClient.connect(err => {
     if (err) {
-        console.error(err);
+        console.error('err occured: ', err);
         process.exit(-1);
     }
     console.log('Successfully connected to Mongo DB...');
 });
 
 module.exports = DbClient;
+
+
+// const mongoose = require('mongoose');
+
+// const dbConnect = () => {
+//     mongoose.connect('mongodb://localhost:27017/practice-final-exam-db', () => console.log('Connected to database'));
+// }
+
+// module.exports = dbConnect;
