5ac1ce28517893695ac014cdb72307e17d4b1ef1
MadhaviImashi <madhaviimashi@gmail.com>
2022-06-26 13:52:08 +0530

    inventory files refactored

--- backend/api/inventory.js
+++ backend/api/inventory.js
@@ -1,56 +1,24 @@
 const Inventory = require('../models/inventory');
-/* -----------------------------------------------------APIs for Inventory(shopping items) related data handling------------------------------------------------- */
-// const uniqid = require('uniqid');
-// let inventory = new Map();
-
-// inventory = [];
-
-// const item1 = { id: uniqid(), name: 'LapTop-Table', price: 10000, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed  " };
-// const item2 = { id: uniqid(), name: 'Office-Chair', price: 2000, price: 10000, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"  };
-// const item3 = { id: uniqid(), name: 'Water bottle', price: 300, price: 10000, description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do "  };
-
-// inventory.push(item1);
-// inventory.push(item2);
-// inventory.push(item3);
 
 const getAll = async () => {
-    // return [...inventory.values()];
-  let allInventories = await Inventory.find();
+  let allInventories = await Inventory.find({});
+  console.log('all', allInventories);
   return allInventories;
 }
 
-const getById = (id) => {
-
-  const selectedItem = Inventory.findById(id);
-  if (!selectedItem) {
-    throw new Error(`Not found for the ID ${id}`);
-  }
-  return selectedItem;
-}
-
-const save = async (data) => {
-  // const item = { id: uniqid(), name: data.name, price: data.price, description: data.description};
+const save = (data) => {
   const item = new Inventory({ name: data.name, price: data.price, description: data.description });
-  // console.log('itemm', item);
-  await item.save();
-
-  return item;
-}
-
-const update = (id, data) => {
-
-  const index = inventory.findIndex(item => item.id === id);
-
-  if (!inventory[index]) {
-    throw new Error(`Not found for the ID ${id}`);
-  }
-  inventory[index] = { id: id, name: data.name, price: data.price, description: data.description };
-  return inventory[index];
+  console.log('itemm', item);
+  item.save()
+    .then((res) => {
+      console.log('res: ', res);
+    })
+    .catch((err) => {
+    console.log('errorr', err)
+  })
 }
 
 module.exports = {
   getAll,
-  getById,
   save,
-  update
 }
